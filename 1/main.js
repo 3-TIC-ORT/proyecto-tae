0752612979a1410641dbde864133c2f57f503d6a
Your Name <you@example.com>
2025-08-12 15:53:40 -0300

    conecciones hechas

--- 1/main.js
+++ 1/main.js
@@ -1,6 +1,6 @@
 let boton1 = document.getElementById("boton1");
 let boton2 = document.getElementById("boton2");
-let boton3 = document.getElementById("boton3");
+
 function adelante(){
     window.location.href = "C:/Users/50416518/Desktop/proyecto-tae/2/index2.html";
 }
@@ -9,12 +9,6 @@ function mostrarCreditos(){
     window.location.href = "C:/Users/50416518/Desktop/proyecto-tae/Creditos/creditos.html";
 }
 
-
-function cerrar(){
-    window.close();
-}
-
 boton1.addEventListener("click", adelante);
 boton2.addEventListener("click", mostrarCreditos);
-boton3.addEventListener("click", cerrar);
 

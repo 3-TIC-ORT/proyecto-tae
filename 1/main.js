c4271be620103cfad4471515bd56b3fbc06dc991
Your Name <you@example.com>
2025-08-13 09:48:39 -0300

    a

--- 1/main.js
+++ 1/main.js
@@ -2,13 +2,12 @@ let boton1 = document.getElementById("boton1");
 let boton2 = document.getElementById("boton2");
 
 function adelante(){
-    window.location.href = "C:/Users/50416518/Desktop/proyecto-tae/2/index2.html";
+    
 }
 
 function mostrarCreditos(){
-    window.location.href = "C:/Users/50416518/Desktop/proyecto-tae/Creditos/creditos.html";
+    
 }
 
 boton1.addEventListener("click", adelante);
 boton2.addEventListener("click", mostrarCreditos);
-

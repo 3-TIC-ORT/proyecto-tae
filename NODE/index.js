5db41afc5ce72ca78d6f379685ab28fb86c6ddd9
Your Name <you@example.com>
2025-08-21 14:27:05 -0300

    Hola

---
+++ NODE/index.js
@@ -0,0 +1,18 @@
+function quepersonaje(numpersonaje){
+    switch(numpersonaje){
+        case 1:
+            console.log("Tu personaje es el warrior")
+            break
+        case 2:
+            console.log("Tu personaje es el mago")
+            break
+        case 3:
+            console.log("Tu personaje es la valkiria")
+            break
+        case 4:
+            console.log("Tu personaje es  el pickpocket")
+            break
+
+    }
+}
+quepersonaje(1)

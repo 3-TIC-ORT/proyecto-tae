f548a8153bfca1e2600c8f8d5fcd488bc1ecefa5
Your Name <you@example.com>
2025-08-26 15:45:14 -0300

    botones (primeros) funcionan y corrección

--- 1/main.js
+++ 1/main.js
@@ -1,12 +1,12 @@
 let boton1 = document.getElementById("boton1");
 let boton2 = document.getElementById("boton2");
 
-function adelante(){
-    
+function adelante() {
+    window.location.href = "../2/index2.html"; 
 }
 
-function mostrarCreditos(){
-    
+function mostrarCreditos() {
+    window.location.href =  "../Creditos/creditos.html";  
 }
 
 boton1.addEventListener("click", adelante);
---
+++ Creditos/main3.js
@@ -0,0 +1,6 @@
+let atras = document.getElementById("flechad");
+
+function volver(){
+    window.location.href ="../1/index.html";
+}
+atras.addEventListener("click", volver)

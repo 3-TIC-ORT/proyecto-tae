8a9a85c4315c13513d56e2243e649b66c2a2699d
Your Name <you@example.com>
2025-08-29 11:58:40 -0300

    todo

---
+++ Game over/main.js
@@ -0,0 +1,7 @@
+let boton1 = document.getElementById ("reintentar");
+let boton2 = document.getElementById ("volver");
+
+function vuelta () {
+window.location.href = "../2/index2.html";
+}
+boton2.addEventListener ("click",vuelta);

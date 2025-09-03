44072d861619d863ab25fc92c9da1ffcf67f75e3
Your Name <you@example.com>
2025-09-03 11:55:11 -0300

    inicio wireframe seleccion personajes (real)

--- 2/2.js
+++ 2/2.js
@@ -1,24 +1,25 @@
 let mago = document.getElementById("magician");
-let valk = document.getElementById("valkiria");
-let oro = document.getElementById("li1");
-let vida = document.getElementById("li2");
+let jon = document.getElementById("jon");
+let bear = document.getElementById("bear");
+let pick = document.getElementById("pick");
 
-function statsmago(){
-    oro.textContent = "Oro: 150";
-    vida.textContent = "Vida: 70";
-    console.log("mago");
+function mostrarBear(){
+    alert("bear")
 }
-function statsvalk(){
-    oro.textContent = "Oro: 125";
-    vida.textContent = "Vida: 25";
-    console.log("valk");
-}
-function ocultar(){
-    oro.textContent = "";
-    vida.textContent = "";
+function mostrarMago(){
+    alert("mago")
 }
 
-valk.addEventListener("mouseover", statsvalk);
-mago.addEventListener("mouseover", statsmago);
-valk.addEventListener("mouseleave", ocultar);
-mago.addEventListener("mouseleave", ocultar);
+function mostrarPick(){
+    alert("pick")
+}
+
+function mostrarJon(){
+    alert("jon")
+}
+
+mago.addEventListener("click", mostrarMago);
+jon.addEventListener("click", mostrarJon);
+bear.addEventListener("click", mostrarBear);
+pick.addEventListener("click", mostrarPick);
+

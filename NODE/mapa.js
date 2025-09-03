38c52d19eab4bceb33b8b7a81713de682034c2d5
Your Name <you@example.com>
2025-09-03 10:46:30 -0300

    MAPA

--- NODE/mapa.js
+++ NODE/mapa.js
@@ -30,45 +30,4 @@ grafo.agregararista("mounstro", "fogata");
 grafo.agregararista("tienda", "mounstro2");
 grafo.agregararista("fogata","mounstro2");
 
-grafo.mostrar();
-
-/*class Grafo {
-    constructor() {
-      this.vertices = {};
-    }
-  
-    agregarVertice(nombre) {
-      if (!this.vertices[nombre]) {
-        this.vertices[nombre] = [];
-      }
-    }
-  
-    agregarArista(v1, v2) {
-      if (!this.vertices[v1] || !this.vertices[v2]) return;
-  
-      this.vertices[v1].push(v2);
-      this.vertices[v2].push(v1); 
-    }
-  
-    mostrar() {
-      for (let vertice in this.vertices) {
-        console.log(`${vertice} -> ${this.vertices[vertice].join(', ')}`);
-      }
-    }
-  }
-  
-  const grafo = new Grafo();
-  
-  grafo.agregarVertice("mounstro");
-  grafo.agregarVertice("tienda");
-  grafo.agregarVertice("fogata");
-  grafo.agregarVertice("mounstro2")
-  
-  grafo.agregarArista("mounstro", "tienda");
-  grafo.agregarArista("mounstro", "fogata");
-  grafo.agregarArista("tienda", "mounstro2");
-  grafo.agregarArista("fogata","mounstro2");
-  
-  grafo.mostrar();
-
-  */
+grafo.mostrar();  

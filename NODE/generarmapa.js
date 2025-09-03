74f1f2e42564cb5d1caaa5cf9923f00dd70221ab
Your Name <you@example.com>
2025-09-03 14:05:47 -0300

    mapa completo

--- NODE/generarmapa.js
+++ NODE/generarmapa.js
@@ -1,17 +1,18 @@
 let grafo = [];
 let conexiones = [];
-let cantidadpisos = 5;
+let cantidadpisos = 10;
+let finalboss = ["Final Boss"]
 function numeroAleatorio() {
     return Math.floor(Math.random() * (5 - 3 + 1)) + 3;
   }
 for(let i = 0;i <= cantidadpisos;i++){
 let piso = []
 for(let z = 1; z <= numeroAleatorio(); z++){
-    piso.push(i+"-"+z)
+    piso.push(i+"-"+z)  
 }
 grafo.push(piso)
 }
-grafo.push(["piso final"])
+grafo.push(finalboss)
 console.log(grafo)
 for (let i = 0; i < grafo.length - 1; i++) {
     let pisoactual = grafo[i];
@@ -21,7 +22,7 @@ for (let i = 0; i < grafo.length - 1; i++) {
         let cantidadConexiones = Math.floor(Math.random() * 2) + 1;
       
         for (let c = 0; c < cantidadConexiones; c++) {
-          let aleatorio = pisosiguiente[Math.floor(Math.random() * 2) + 1];
+          let aleatorio = pisosiguiente[Math.floor(Math.random() * pisosiguiente.length)];
           conexiones.push([nodo, aleatorio]);
         }
       }

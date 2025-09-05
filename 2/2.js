c898443ed4b1295bc4413b33eabe3e6a12e4e80b
Your Name <you@example.com>
2025-09-05 12:06:36 -0300

    JS seleccion personajes mejor

--- 2/2.js
+++ 2/2.js
@@ -2,24 +2,38 @@ let mago = document.getElementById("magician");
 let jon = document.getElementById("jon");
 let bear = document.getElementById("bear");
 let pick = document.getElementById("pick");
+let body = document.body;
+let parrafo = document.getElementById("p")
+let h2 = document.getElementById("h2")
+
+
+
 
 function mostrarBear(){
-    alert("bear")
+    body.style.backgroundColor = "brown"
+    parrafo.textContent = "Hola mundo";
+    h2.textContent = "Hola mundo";
 }
 function mostrarMago(){
-    alert("mago")
+    body.style.backgroundColor = "purple"
 }
+
 
 function mostrarPick(){
-    alert("pick")
+    body.style.backgroundColor = "grey"
 }
 
+
 function mostrarJon(){
-    alert("jon")
+    body.style.backgroundColor = "gold"
 }
+
 
 mago.addEventListener("click", mostrarMago);
 jon.addEventListener("click", mostrarJon);
 bear.addEventListener("click", mostrarBear);
 pick.addEventListener("click", mostrarPick);
+
+
+
 

283ffd9f52363469b1b06eff7a8a8fb569fbc09d
Your Name <you@example.com>
2025-09-09 14:10:22 -0300

    seleccion personajes

--- 2/2.js
+++ 2/2.js
@@ -3,37 +3,47 @@ let jon = document.getElementById("jon");
 let bear = document.getElementById("bear");
 let pick = document.getElementById("pick");
 let body = document.body;
-let parrafo = document.getElementById("p")
-let h2 = document.getElementById("h2")
+let parrafo = document.getElementById("p");
+let h1 = document.getElementById("h1");
+let imagen = document.getElementById("imagen-contenido");
 
-
-
-
-function mostrarBear(){
-    body.style.backgroundColor = "brown"
-    parrafo.textContent = "Hola mundo";
-    h2.textContent = "Hola mundo";
-}
-function mostrarMago(){
-    body.style.backgroundColor = "purple"
+function mostrarBear() {
+  body.style.backgroundColor = "brown";
+  h1.textContent = "The Bear";
+  parrafo.textContent =
+    "El oso es una criatura de las montañas de Córdoba que le encanta picar piedra y se alimenta a base de monstruos y elixires. Su hobby principal es programar.";
+  imagen.src = "../Cosas/bear.png";
+  imagen.alt = "foto bear";
 }
 
-
-function mostrarPick(){
-    body.style.backgroundColor = "grey"
+function mostrarMago() {
+  body.style.backgroundColor = "purple";
+  h1.textContent = "The Magician";
+  parrafo.textContent =
+    "El mago domina los secretos de la magia ancestral y controla hechizos poderosos.";
+  imagen.src = "../Cosas/magician.png";
+  imagen.alt = "foto mago";
 }
 
-
-function mostrarJon(){
-    body.style.backgroundColor = "gold"
+function mostrarPick() {
+  body.style.backgroundColor = "grey";
+  h1.textContent = "The Pick";
+  parrafo.textContent =
+    "Pick es un minero legendario que cava en las profundidades de las cuevas buscando tesoros.";
+  imagen.src = "../Cosas/valkiria.png";
+  imagen.alt = "foto valkiria";
 }
 
+function mostrarJon() {
+  body.style.backgroundColor = "gold";
+  h1.textContent = "Jon the Brave";
+  parrafo.textContent =
+    "Jon es un guerrero audaz que nunca teme enfrentarse a dragones y demonios.";
+  imagen.src = "../Cosas/pngtree-mine-cave-entrance-with-railway-in-cartoon-style-isolated-on-white-background-picture-image_7836835.png";
+  imagen.alt = "foto john";
+}
 
 mago.addEventListener("click", mostrarMago);
 jon.addEventListener("click", mostrarJon);
 bear.addEventListener("click", mostrarBear);
 pick.addEventListener("click", mostrarPick);
-
-
-
-

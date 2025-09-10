e62032a8d113c56a7887df0241e51950c48cd605
Your Name <you@example.com>
2025-09-10 12:09:07 -0300

    avances

--- NODE/prueba.js
+++ NODE/prueba.js
@@ -6,7 +6,7 @@ console.log(data)
 } 
 let pisos = input.value;
 boton.addEventListener("click", () => {if(pisos <= 0){alert("la cantidad de pisos no puede ser menor o igual a 0")}});
-getEvent(`mapa?pisos=${pisos}`, (data) => {
+getEvent(`mapa?cantidadpisos=${pisos}`, (data) => {
     console.log("Mapa generado:", data);
-    salida.innerText = JSON.stringify(data);
+    salida.innerText = JSON.stringify(data, null , 2);
 });

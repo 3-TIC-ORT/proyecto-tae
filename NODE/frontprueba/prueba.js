e33280a7e7969e18b0eec7e324a8ba002d2b9622
JulianGarbate <juliangarbate@gmail.com>
2025-10-04 00:53:21 -0300

    avances y cracion de ruta de reliquias y cartas con verificacion

---
+++ NODE/frontprueba/prueba.js
@@ -0,0 +1,52 @@
+connect2Server()
+let boton = document.getElementById("boton");
+let input = document.getElementById("input");
+let salida = document.getElementById("salida")
+let guardar = document.getElementById("guardar")
+let mazo = document.getElementById("mazo")
+let carta = document.getElementById("carta")
+let agregar = document.getElementById("agregar")
+let eliminar = document.getElementById("eliminar")
+let reliquias = document.getElementById("reliquias")
+let reliquia = document.getElementById("reliquia")
+let agregarreliquia = document.getElementById("agregar-reliquia")
+function mapa(data){
+    console.log(data)
+} 
+reliquias.addEventListener("click",() => {
+    getEvent("reliquia",(data)=>{
+        salida.innerText = JSON.stringify(data,null,2)
+    })
+}) 
+agregarreliquia.addEventListener("click",()=>{
+    console.log("modificando reliquias")
+    postEvent("agregar-reliquia",reliquia.value)
+})
+
+agregar.addEventListener("click",()=>{
+    console.log("modificando mazo")
+    postEvent("modificar-mazo",{accion:"agregar",carta:carta.value})
+})
+eliminar.addEventListener("click",()=>{
+    console.log("modificando mazo")
+    postEvent("modificar-mazo",{accion:"eliminar",carta:carta.value})
+})
+mazo.addEventListener("click",() => {
+    getEvent("mazo",(data)=>{
+        salida.innerText = JSON.stringify(data,null,2)
+    })
+})  
+boton.addEventListener("click", () => {
+    let pisos = parseInt(input.value);
+    if (pisos <= 0) {
+        alert("La cantidad de pisos no puede ser menor o igual a 0");
+        return;
+    }
+
+    getEvent(`mapa?cantidadpisos=${pisos}`, (data) => {
+        console.log("Mapa generado:", data);
+        salida.innerText = JSON.stringify(data, null, 2);
+    });
+});
+guardar.addEventListener("click",() => {
+    postEvent("guardar",true)})
--- saving.js
+++ saving.js
@@ -1,5 +1,4 @@
 import { subscribeGETEvent, subscribePOSTEvent, realTimeEvent, startServer } from "soquetic";
-import {cartas,reliquias} from "./NODE/datosprueba.js"
 import { generarmapa } from './NODE/mapa/generarmapa.js';
 import fs from "fs";
 let info = {
@@ -10,11 +9,11 @@ let info = {
       vidamax:0,
       oro:0,
     },
-    mazo:cartas,
+    mazo:null,
     mapa:null,
-    reliquias:reliquias
+    reliquias:null
 }
-console.log(info.mapa)
+
 subscribeGETEvent("mapa",(query) => {
   const mapa = generarmapa(query)
   info.mapa = mapa
@@ -84,4 +83,64 @@ subscribePOSTEvent("guardar",(data) => {
   fs.writeFileSync("./jsons/saving.json",JSON.stringify(saving,null,2))
 }
 })
+
+subscribeGETEvent("mazo",() => {
+    let mazo = JSON.parse(fs.readFileSync("./NODE/jsons/mazo.json"))
+    info.mazo = mazo
+    console.log(mazo)
+    return mazo
+})
+
+subscribePOSTEvent("modificar-mazo", (data) => {
+  console.log("POST recibido:", data);
+
+  let mazo = JSON.parse(fs.readFileSync("./NODE/jsons/mazo.json", "utf-8"));
+  let cartas = JSON.parse(fs.readFileSync("./NODE/jsons/cartas.json", "utf-8"));
+
+  let carta = cartas.find(carta => carta.nombre.toLowerCase() === data.carta.toLowerCase());
+
+  if(!carta) {
+      console.log("Carta no encontrada:", data.carta);
+      return { error: "Carta no encontrada" };
+  }
+
+  if(data.accion === "agregar"){
+      mazo.push(carta);
+
+  } else if(data.accion === "eliminar"){
+      let index = mazo.findIndex(carta => carta.nombre === carta.nombre);
+      if(index !== -1) mazo.splice(index, 1);
+  }
+
+  fs.writeFileSync("./NODE/jsons/mazo.json", JSON.stringify(mazo, null, 2));
+  return mazo;
+});
+
+subscribeGETEvent("reliquia",() => {
+    let reliquias = JSON.parse(fs.readFileSync("./NODE/jsons/reliquiauso.json","utf-8"))
+    info.reliquias = reliquias
+    console.log(reliquias)
+    return reliquias
+})
+
+subscribePOSTEvent("agregar-reliquia", (data) => {
+  console.log("POST recibido:", data);
+
+  let reliquiasuso = JSON.parse(fs.readFileSync("./NODE/jsons/reliquiauso.json", "utf-8"));
+  let reliquias = JSON.parse(fs.readFileSync("./NODE/jsons/reliquias.json", "utf-8"));
+
+  let reliquia = reliquias.find(r => r.nombre.toLowerCase() === data.toLowerCase());
+
+  if(!reliquia) {
+      console.log("Carta no encontrada:", data);
+      return { error: "Carta no encontrada" };
+  }
+  else{
+      reliquiasuso.push(reliquia);
+
+  }
+  fs.writeFileSync("./NODE/jsons/reliquiauso.json", JSON.stringify(reliquiasuso, null, 2));
+  return reliquiasuso;
+});
+
 startServer();

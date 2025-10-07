6990bf235cb33737990949d1900b5670df4bbc8e
Joaco <joacoschop@gmail.com>
2025-10-07 16:29:48 -0300

    boton sumar y sacar cartas
    Co-authored-by: JulianGarbate <JulianGarbate@users.noreply.github.com>

--- Front/batalla/main.js
+++ Front/batalla/main.js
@@ -1,35 +1,63 @@
 window.addEventListener("DOMContentLoaded", () => {
-    let oro = document.getElementById("oro");
-    let vida = document.getElementById("vida");
-    let info = {};
-    let mapa = document.getElementById("mapa");
+  let oro = document.getElementById("oro");
+  let vida = document.getElementById("vida");
+  let info = {};
+  let mapa = document.getElementById("mapa");
+  let boton = document.getElementById("buton");
+  let botonSacar = document.getElementById("butonSacar");
+  let abajo = document.getElementById("abajo");
 
-    connect2Server();
+  let contadorCartas = 1; // empieza en carta1
 
-    getEvent("fogata", (data) => {
-        info = {
-            oro: data.oro,
-            vida: data.vida,
-            vidamax: data.vidamax,
-            mapa: data.mapa,
-            reliquias: data.reliquias
-        };
+  connect2Server();
 
-        // 👇 Mostrar recién acá, cuando ya tenemos los datos
-        mostrarOro();
-        mostrarVida();
-    });
+  getEvent("fogata", (data) => {
+    info = {
+      oro: data.oro,
+      vida: data.vida,
+      vidamax: data.vidamax,
+      mapa: data.mapa,
+      reliquias: data.reliquias,
+    };
+    mostrarOro();
+    mostrarVida();
+  });
 
-    function mostrarVida() {
-        vida.textContent = "PV: " + info.vida + "/" + info.vidamax;
+  function mostrarVida() {
+    vida.textContent = "PV: " + info.vida + "/" + info.vidamax;
+  }
+
+  function mostrarOro() {
+    oro.textContent = "Oro: " + info.oro;
+  }
+
+  function irMapa() {
+    window.location.href = "../mapa/index.html";
+  }
+
+  mapa.addEventListener("click", irMapa);
+
+  function sumarCarta() {
+    if (contadorCartas > 9) return;
+
+    let carta = document.createElement("div");
+    carta.classList.add(`carta${contadorCartas}`, "cartaG");
+    abajo.appendChild(carta);
+
+    contadorCartas++;
+  }
+
+  function sacarCarta() {
+    if (contadorCartas <= 1) return;
+
+    contadorCartas--;
+
+    let cartaAEliminar = abajo.querySelector(`.carta${contadorCartas}`);
+    if (cartaAEliminar) {
+      cartaAEliminar.remove();
     }
+  }
 
-    function mostrarOro() {
-        oro.textContent = "Oro: " + info.oro;
-    }
-    function irMapa() {
-        window.location.href = "../mapa/index.html";
-      }
-
-    mapa.addEventListener("click", irMapa);
+  boton.addEventListener("click", sumarCarta);
+  botonSacar.addEventListener("click", sacarCarta);
 });
